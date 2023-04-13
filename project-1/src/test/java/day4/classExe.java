8e34bb8eb37dbce77826fc6cb2b0c87cf1572e4b
karthikeyan508 <125950930+karthikeyan508@users.noreply.github.com>
2023-04-13 12:44:23 +0530

    Add files via upload

---
+++ project-1/src/test/java/day4/classExe.java
@@ -0,0 +1,20 @@
+package day4;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+public class classExe {
+		public static void main(String args[]) {
+			WebDriverManager.chromedriver().setup();
+			WebDriver driver;
+			ChromeOptions co = new ChromeOptions();
+			co.addArguments("--remote-allow-origins=*");
+			driver = new ChromeDriver(co);
+			
+			driver.manage().window().maximize();
+			driver.navigate().to("https://www.amazon.in/?&ext_vrnc=hi&tag=googhydrabk1-21&ref=pd_sl_7hz2t19t5c_e&adgrpid=58355126069&hvpone=&hvptwo=&hvadid=486458706470&hvpos=&hvnetw=g&hvrand=5941688106302313868&hvqmt=e&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=1007810&hvtargid=kwd-10573980&hydadcr=14453_2154373");
+		}
+}
+		
